2bffdaacab2f3d2efbfe0266ec881d20dae53a9b
Zhicheng Chen <chenzhicheng@dayuwuxian.com>
2020-01-13 12:12:04 +0800

    init

---
+++ init.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mv curriculum/challenges/chinese/02-javascript-algorithms-and-data-structures/basic-data-structures/-iterate-through-the-keys-of-an-object-with-a-for...in-statement.chinese.md curriculum/challenges/chinese/02-javascript-algorithms-and-data-structures/basic-data-structures/iterate-through-the-keys-of-an-object-with-a-for...in-statement.chinese.md
+mv curriculum/challenges/chinese/05-apis-and-microservices/mongodb-and-mongoose/use-model.findbyid-to-search-your-database-by-_id.chinese.md curriculum/challenges/chinese/05-apis-and-microservices/mongodb-and-mongoose/use-model.findbyid-to-search-your-database-by-id.chinese.md
+mv curriculum/challenges/chinese/03-front-end-libraries/react/render-with-an-ifelse-condition.chinese.md curriculum/challenges/chinese/03-front-end-libraries/react/render-with-an-if-else-condition.chinese.md
